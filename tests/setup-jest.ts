48a5bcf3180de9e96c95be47ddfd1ad03b1211c4
zoutigo <zoutigo@gmail.com>
2026-01-19 10:58:34 +0100

    feature: stantardized components

--- tests/setup-jest.ts
+++ tests/setup-jest.ts
@@ -2,3 +2,11 @@ import "@testing-library/jest-dom";
 import { TextDecoder, TextEncoder } from "util";
 
 Object.assign(global, { TextEncoder, TextDecoder });
+
+beforeAll(() => {
+	jest.spyOn(console, "warn").mockImplementation(() => {});
+});
+
+afterAll(() => {
+	jest.restoreAllMocks();
+});
